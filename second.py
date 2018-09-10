f9084e6869facef9c3946d77563fa167db3c951d
Emily Huang <emilyhua.11573@redlandsschools.net>
2018-09-10 15:25:38 +0000

    age

---
+++ second.py
@@ -0,0 +1,9 @@
+"""
+Triple QUOTES will allow me to comment out multiple lines
+
+Write a program that will ask the user what their age is and then determine if they are old enough to vote or no and respond appropriately
+
+"""
+
+x = input('How old are you?')
+print ("Since you are" + str(x) + "," "and you can vote, Who are you voting for?")
